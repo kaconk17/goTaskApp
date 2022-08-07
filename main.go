4cc45e1cfe6689d85a69502fc17e4cf4ed65c836
kaconk <harutonouciha@gmail.com>
2022-08-07 07:03:12 +0700

    fiber html render

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"database/sql"
 	"fmt"
+
+	//"html"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
@@ -18,18 +21,39 @@ func main() {
 		log.Fatalf("Error loading .env file")
 	  }
 	host := os.Getenv("HOST")
+	dbPort := os.Getenv("DB_PORT")
 	database := os.Getenv("DATABASE")
-	user := os.Getenv("USER")
-	pass := os.Getenv("PASSWORD")
+	dbuser := os.Getenv("DB_USER")
+	dbpass := os.Getenv("DB_PASSWORD")
 
-	connStr := "postgresql://"+user+":"+pass+"@"+host+"/"+database+"?sslmode=disable"
+	
+	connStr := "postgresql://"+dbuser+":"+dbpass+"@"+host+":"+dbPort+"/"+database+"?sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
-	app := fiber.New()
+	defer db.Close()
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 	app.Get("/", func(c *fiber.Ctx) error {
 		return indexHandler(c,db)
+	})
+	app.Get("/all", func(c *fiber.Ctx) error {
+		return getAll(c,db)
+	})
+	app.Post("/add", func(c *fiber.Ctx) error {
+		return postHandler(c, db)
+	})
+
+	app.Put("/update", func(c *fiber.Ctx) error {
+		return putHandler(c, db)
+	})
+
+	app.Delete("/delete", func(c *fiber.Ctx) error {
+		return deleteHandler(c, db)
 	})
 	
    port := os.Getenv("PORT")
@@ -41,5 +65,71 @@ func main() {
 }
 
 func indexHandler(c *fiber.Ctx, db *sql.DB) error {
-	return c.SendString("Hello")
+	
+	var res string
+	var tasks []string
+
+	rows, err := db.Query("select * from tb_task")
+	
+	if err != nil {
+		log.Fatalln(err)
+		c.JSON("Terjadi kesalahan")
+	}
+
+	for rows.Next() {
+		rows.Scan(&res)
+		tasks = append(tasks, res)
+	}
+	return c.Render("index", fiber.Map{
+		"Tasks":tasks,
+	})
+	
 }
+
+func getAll(c *fiber.Ctx, db *sql.DB) error {
+
+	return c.JSON("data")
+}
+type newtask struct {
+	nama string;
+	isi string;
+	tanggal string;
+}
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
+	task := newtask{}
+	type setatus struct {
+		success bool;
+		pesan string;
+	}
+	
+	hasil := setatus{
+		success: false,
+		pesan: "Error",
+	}
+	if err := c.BodyParser(&task); err != nil {
+		log.Printf("An error occured: %v", err)
+		return c.JSON(err.Error())
+	}
+	if task.isi != "" {
+		_, err := db.Exec("INSERT into tb_task VALUES ($1,$2,$3)", task.nama, task.isi, task.tanggal)
+		if err != nil {
+			log.Fatalf("An error occured while executing query: %v", err)
+		}
+		
+		hasil = setatus{
+			success: true,
+			pesan: "Insert success",
+		}
+
+	}
+	
+	return c.JSON(hasil)
+ }
+
+ func putHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+ }
+
+ func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+ }
